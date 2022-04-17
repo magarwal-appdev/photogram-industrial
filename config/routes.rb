73de388d9e34bd46c68fcf3232a1563b08c45633
Megha-Avee <megha@neonforlife.com>
2022-04-17 23:40:43 +0000

    added own photos to users#show

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
+  root "photos#index"
+
+  devise_for :users
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
   resources :likes
   resources :follow_requests
   resources :comments
   resources :photos
-  devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root "photos#index"
+
+  # get "/users/:id" => "users#show", as: :user
+  resources :users, only: [:show]
+  
 end
