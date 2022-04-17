7e4f22b4ed9377b1181d2f3e85323f77c7e3189b
Megha-Avee <megha@neonforlife.com>
2022-04-17 01:13:49 +0000

    created photos

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_16_174630) do
+ActiveRecord::Schema.define(version: 2022_04_17_010824) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
@@ -18,8 +18,8 @@ ActiveRecord::Schema.define(version: 2021_04_16_174630) do
 
   create_table "photos", force: :cascade do |t|
     t.string "image"
-    t.integer "comments_count", default: 0
-    t.integer "likes_count", default: 0
+    t.integer "comments_count"
+    t.integer "likes_count"
     t.text "caption"
     t.bigint "owner_id", null: false
     t.datetime "created_at", precision: 6, null: false
@@ -28,7 +28,7 @@ ActiveRecord::Schema.define(version: 2021_04_16_174630) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.citext "email", default: "", null: false
+    t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
@@ -36,7 +36,7 @@ ActiveRecord::Schema.define(version: 2021_04_16_174630) do
     t.citext "username"
     t.boolean "private"
     t.integer "likes_count", default: 0
-    t.integer "comments_count", default: 0
+    t.integer "comments_count"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
